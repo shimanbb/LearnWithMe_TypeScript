0086b2bb49a03b23de049f0a170bc56357178e4c
Debashish Paul <debashishpaul.itm@gmail.com>
2016-02-01 16:52:57 +1100

    Optional Type Notation

--- 01_NutsNBolts/types.js
+++ 01_NutsNBolts/types.js
@@ -1,3 +1,14 @@
 //Inference
 var test = 0;
-//test = "Sorry mate, this won't compile";
+
+//test = "Sorry mate, this won't compile";  //Cannot convert 'string' to 'number'
+//Optional Static Notation
+var num;
+var str;
+
+/*if(1 != 2)
+{
+num = "This won't compile"; //Cannot convert 'string' to 'number'
+}*/
+num = 0;
+str = "Hello World!";
