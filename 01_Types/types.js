3a44c83b861a3c7bcd6ff02a85f21ab28b675d76
Debashish Paul <debashishpaul.itm@gmail.com>
2016-02-08 17:33:14 +1100

    #1 - Types: Any and Enum Type

---
+++ 01_Types/types.js
@@ -0,0 +1,35 @@
+var test = 0;
+var num;
+var str;
+num = 0;
+str = "Hello World!";
+var anything;
+var implicit_anything;
+anything = -1;
+anything = [1, 2, 3];
+anything = "This will be compiled";
+implicit_anything = -1;
+implicit_anything = [1, 2, 3];
+implicit_anything = "This will be compiled too";
+var arr1 = [1, 2, 3];
+var arr2 = [1, 2, 3];
+var MyEnum1;
+(function (MyEnum1) {
+    MyEnum1[MyEnum1["Val1"] = 0] = "Val1";
+    MyEnum1[MyEnum1["Val2"] = 1] = "Val2";
+    MyEnum1[MyEnum1["Val3"] = 2] = "Val3";
+})(MyEnum1 || (MyEnum1 = {}));
+;
+var enumVal1 = MyEnum1.Val1;
+var MyEnum2;
+(function (MyEnum2) {
+    MyEnum2[MyEnum2["Val1"] = 11] = "Val1";
+    MyEnum2[MyEnum2["Val2"] = 12] = "Val2";
+    MyEnum2[MyEnum2["Val3"] = 13] = "Val3";
+})(MyEnum2 || (MyEnum2 = {}));
+;
+var enumVal2 = MyEnum2.Val2;
+var unionVariable;
+unionVariable = 2;
+unionVariable = 'Hello World';
+unionVariable = ["Hello", "World"];
---
+++ 01_Types/types.ts
@@ -0,0 +1,45 @@
+//Inference
+var test = 0;
+//test = "Sorry mate, this won't compile";  //Cannot convert 'string' to 'number'
+
+//Optional Static Notation
+var num: number;
+var str: string;
+
+/*if(1 != 2)
+{
+  num = "This won't compile"; //Cannot convert 'string' to 'number'
+}*/
+
+num = 0;
+str = "Hello World!";
+
+//Any
+var anything: any; //explicite any
+var implicit_anything; //implicite any
+
+anything = -1;
+anything = [1, 2, 3]; //No problem
+anything = "This will be compiled";
+
+implicit_anything = -1;
+implicit_anything = [1, 2, 3]; //No problem
+implicit_anything = "This will be compiled too";
+
+//Array
+var arr1: number[] = [1, 2, 3];
+var arr2: Array<number> = [1, 2, 3];
+
+//Enum
+enum MyEnum1 {Val1, Val2, Val3};
+var enumVal1: MyEnum1 = MyEnum1.Val1;
+
+enum MyEnum2 {Val1 = 11, Val2, Val3};
+var enumVal2: MyEnum2 = MyEnum2.Val2;
+
+//Union Type
+var unionVariable: number | string | string[];
+unionVariable = 2;
+unionVariable = 'Hello World';
+unionVariable = ["Hello", "World"];
+//unionVariable = true;
